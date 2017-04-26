9b2425ffd46ee721be1dd341a6feecc4347bb52d
Yannick Guth <yannick.guth@hs-bochum.de>
2017-04-26 13:28:26 +0200

    added sleuth for log tracing

--- src/main/java/de/cluster/microservices/tickets/controller/TicketController.java
+++ src/main/java/de/cluster/microservices/tickets/controller/TicketController.java
@@ -2,6 +2,8 @@ package de.cluster.microservices.tickets.controller;
 
 import de.cluster.microservices.tickets.model.Ticket;
 import de.cluster.microservices.tickets.service.TicketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Consumes(APPLICATION_JSON)
 @RestController
 public class TicketController {
+
+    private static Logger LOG = LoggerFactory.getLogger(TicketController.class);
 
 	 @Autowired
 	 private TicketService ticketService;
@@ -58,6 +62,7 @@ public class TicketController {
     @RequestMapping(value = "/tickets/{id}", method = RequestMethod.GET)
     public ResponseEntity<Ticket> getTicketById(
         @PathVariable String id) {
+        LOG.info("* get ticket by id *");
         Ticket t = ticketService.getTicket(id);
         if(t == null) {
             return new ResponseEntity<>((Ticket) null,HttpStatus.NOT_FOUND);
@@ -70,6 +75,7 @@ public class TicketController {
      */
     @RequestMapping(value = "/tickets", method = RequestMethod.GET)
 	public ResponseEntity<List<Ticket>> getTickets() {
+        LOG.info("* get all tickets *");
 		List<Ticket> tickets = ticketService.getTickets();
 		ResponseEntity<List<Ticket>> tRes = new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
 		return tRes;
